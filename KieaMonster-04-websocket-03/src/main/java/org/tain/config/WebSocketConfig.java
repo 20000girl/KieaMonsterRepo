a134ae8efc186ce59a706805ed6a4ab63456b632
gksruf2848 <4455qqq@naver.com>
2021-03-30 14:40:42 +0900

    2021 03 30

---
+++ KieaMonster-04-websocket-03/src/main/java/org/tain/config/WebSocketConfig.java
@@ -0,0 +1,34 @@
+package org.tain.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws").withSockJS();
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.setApplicationDestinationPrefixes("/app");
+
+		// Enables a simple in-memory broker
+		registry.enableSimpleBroker("/topic");
+
+		/*
+		// Use this for enabling a full featured broker like RabbitMQ
+		registry.enableStompBrokerRelay("/topic")
+			.setRelayHost("localhost")
+			.setRelayPort(61613)
+			.setClientLogin("guest")
+			.setClientPasscode("guest");
+		*/
+	}
+}
